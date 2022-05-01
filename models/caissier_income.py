482ba37d7bf4fffb479dda4ae6133bb4524a61eb
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-05-01 10:49:08 +0100

    update: add spending and income model

--- models/caissier_income.py
+++ models/caissier_income.py
@@ -7,7 +7,34 @@ class Openning(models.Model):
  _description = "Caissier Income"
  time = fields.Char(string="ساعة الافتتاح",default=datetime.now().strftime("%H:%M:%S"))
  date = fields.Date("تاريخ الافتتاح")
+ 
+ 
  balance=fields.Integer(string="الرصيد", readonly=True,default=0)
+ 
+ 
+ first=fields.Integer(string="الرصيد", readonly=True,default=0)
+ first_qty=fields.Integer(string="الرصيد", readonly=True,default=0) 
+ 
+ 
+ 
+ seconde_qty=fields.Integer(string="الرصيد", readonly=True,default=0)
+ seconde=fields.Integer(string="الرصيد", readonly=True,default=0)
+ 
+ 
+ 
+ tree_qty=fields.Integer(string="الرصيد", readonly=True,default=0) 
+ tree=fields.Integer(string="الرصيد", readonly=True,default=0)
+ 
+
+ four_qty=fields.Integer(string="الرصيد", readonly=True,default=0)
+ four=fields.Integer(string="الرصيد", readonly=True,default=0)
+ 
+ 
+ 
+ 
+ operation_code=fields.Char(string="")
+ client_code=fields.Integer(string="")
+
 
 
 
--- models/caissier_spending.py
+++ models/caissier_spending.py
@@ -1,15 +1,20 @@
 from odoo import fields, models
 from datetime import datetime,date
 
-
+doors=[('option1', 'Label 1'), ('option2', 'Label 2')]
+entrances=[('option1', 'Label 1'), ('option2', 'Label 2')]
 class Openning(models.Model):
  _name = "caissier.spending"
  _description = "Caissier Spending"
  time = fields.Char(string="ساعة الافتتاح",default=datetime.now().strftime("%H:%M:%S"))
  date = fields.Date("تاريخ الافتتاح")
- balance=fields.Integer(string="الرصيد", readonly=True,default=0)
 
 
+
+ door= fields.Selection(doors, string='My Selection Field')
+
+
+ entrance= fields.Selection(entrances, string='My Selection Field')
 
  def button_check_isbn(self):
     print("Hi  Hi HI")
